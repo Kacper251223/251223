c2404ff909733ccea2a28220cf559d32f7690bfe
Kacper251223 <251223@edu.p.lodz.pl>
2024-01-29 13:25:10 +0100

    initial

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int policz (int a,int b){
-    return a+b;
+    return a*b;
 }
 
 int main() {
 
     printf("hello world!\n");
-    printf("w pierwrwszym branchu; 3+2=%d", policz(3,2));
+    printf("w pierwrwszym branchu; 3*2=%d", policz(3,2));
 }
